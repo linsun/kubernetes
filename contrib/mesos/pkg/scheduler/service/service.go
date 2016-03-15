6cdbce83f4e7fd42866611a071535e35ad0f62a0
Lin Sun <linsun@us.ibm.com>
2016-03-15 15:14:03 -0400

    merge pull req 349 manually

--- contrib/mesos/pkg/scheduler/service/service.go
+++ contrib/mesos/pkg/scheduler/service/service.go
@@ -50,6 +50,7 @@ import (
 
 	"k8s.io/kubernetes/contrib/mesos/pkg/election"
 	execcfg "k8s.io/kubernetes/contrib/mesos/pkg/executor/config"
+	"k8s.io/kubernetes/contrib/mesos/pkg/flagutil"
 	"k8s.io/kubernetes/contrib/mesos/pkg/hyperkube"
 	minioncfg "k8s.io/kubernetes/contrib/mesos/pkg/minion/config"
 	"k8s.io/kubernetes/contrib/mesos/pkg/podutil"
@@ -495,6 +496,10 @@ func (s *SchedulerServer) prepareExecutorInfo(hks hyperkube.Interface) (*mesos.E
 	appendOptional("host-network-sources", s.kubeletHostNetworkSources)
 	appendOptional("network-plugin", s.kubeletNetworkPluginName)
 
+ 	// TODO(jdef) this code depends on poorly scoped cadvisor flags, will need refactoring soon
+ 	appendOptional(flagutil.Cadvisor.HousekeepingInterval.NameValue())
+ 	appendOptional(flagutil.Cadvisor.GlobalHousekeepingInterval.NameValue())
+ 
 	log.V(1).Infof("prepared executor command %q with args '%+v'", ci.GetValue(), ci.Arguments)
 
 	// Create mesos scheduler driver.
